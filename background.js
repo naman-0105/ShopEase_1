d880c4743df10738cd5b638befdb867dec5bf90a
Naman Goyal <147164659+naman-0105@users.noreply.github.com>
2024-07-24 12:40:02 +0530

    Updated Repo

--- background.js
+++ background.js
@@ -17,41 +17,4 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log("Hushh extension installed.");
 });
 
-const trackIntervals = {};
-
-function startRecordingHistory(product) {
-    const interval = setInterval(() => {
-        chrome.storage.local.get({ trackHistory: {} }, (result) => {
-            const trackHistory = result.trackHistory || {};
-            const history = trackHistory[product.url] || [];
-
-            
-            history.push({
-                price: product.price,
-                time: new Date().toLocaleTimeString(),
-                date: new Date().toLocaleDateString()
-            });
-
-            trackHistory[product.url] = history;
-
-            chrome.storage.local.set({ trackHistory });
-        });
-    }, 10000); 
-
-    trackIntervals[product.url] = interval;
-}
-
-function stopRecordingHistory(productUrl) {
-    clearInterval(trackIntervals[productUrl]);
-    delete trackIntervals[productUrl];
-}
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'startTracking') {
-        startRecordingHistory(request.product);
-        sendResponse({ status: 'tracking started' });
-    } else if (request.action === 'stopTracking') {
-        stopRecordingHistory(request.productUrl);
-        sendResponse({ status: 'tracking stopped' });
-    }
-});
+  
